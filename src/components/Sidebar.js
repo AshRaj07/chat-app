5d580e1a3bb77839ca209f7e3b4aa820996fda2e
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-08-07 23:49:21 +0530

    Created Chat room list frontend

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Divider } from "rsuite";
 import CreateRoomBtnModal from "./dashboard/CreateRoomBtnModal";
 import DashBoardToggle from "./dashboard/DashBoardToggle";
+import ChatRoomList from "./rooms/ChatRoomList";
 
 const Sidebar = () => {
+  const topSideBarRef = useRef(null);
+  const [height, setheight] = useState(0)
+  useEffect(()=>{
+    if(topSideBarRef.current){
+      setheight(topSideBarRef.current.scrollHeight)
+    }
+  },[topSideBarRef])
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSideBarRef}>
         <DashBoardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join conversation</Divider>
       </div>
+        <ChatRoomList aboveElementHeight={height} />
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,9 @@ import Sidebar from "../components/Sidebar";
 const Home = () => {
   return (
     <Grid fluid className="h-100">
-      <Row>
-        <Col xs={24} md={8}>
-          <Sidebar />
+      <Row className="h-100">
+        <Col className="h-100" xs={24} md={8}>
+          <Sidebar  />
         </Col>
       </Row>
     </Grid>
