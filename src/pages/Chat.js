1a772eac0f5b4858c9a17330ff86aaa4c4d53267
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-08-08 01:56:13 +0530

    created layout for chat page

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router";
+import { Loader } from "rsuite";
+import ChatBottom from "../components/chat-window/bottom";
+import Messages from "../components/chat-window/messages";
+import ChatTop from "../components/chat-window/top";
+import { useRooms } from "../context/rooms.context";
 
 const Chat = () => {
+  const rooms = useRooms();
+  const { chatId } = useParams();
+  if (!rooms) {
+    return <Loader center size="md" content="Loading..." speed="normal" />;
+  }
+  const currentRoom = rooms.find((room) => room.id === chatId);
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
   return (
-    <div>Chat</div>
-  )
-}
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
+};
 
-export default Chat
+export default Chat;
