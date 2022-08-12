841c2958e1582b6cbf643bbdf093d1d7981aeb8d
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-08-12 12:15:17 +0530

    handle delete operation

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,68 +1,72 @@
-import React, { memo } from 'react'
-import { Button } from 'rsuite';
-import TimeAgo from 'timeago-react';
-import { useCurrentRoom } from '../../../context/current-room.context';
-import { useHover, useMediaQuery } from '../../../misc/custom-hook';
-import { auth } from '../../../misc/firebase';
-import ProfileAvatar from '../../dashboard/ProfileAvatar';
-import PresenceDot from '../../PresenceDot';
-import IconBtnControl from './IconBtnControl';
-import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import React, { memo } from "react";
+import { Button } from "rsuite";
+import TimeAgo from "timeago-react";
+import { useCurrentRoom } from "../../../context/current-room.context";
+import { useHover, useMediaQuery } from "../../../misc/custom-hook";
+import { auth } from "../../../misc/firebase";
+import ProfileAvatar from "../../dashboard/ProfileAvatar";
+import PresenceDot from "../../PresenceDot";
+import IconBtnControl from "./IconBtnControl";
+import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
 
-const MessageItem = ({message,handleAdmin,handleLike}) => {
-    const {author,createdAt,text,likes,likeCount} = message;
-    const isAdmin = useCurrentRoom(v=>v.isAdmin)
-    const admins = useCurrentRoom(v=>v.admins)
-    const isMsgAuthorAdmin = admins.includes(author.uid);
-    const isAuthor = auth.currentUser.uid === author.uid;
-    const canGrantAdmin = isAdmin && !isAuthor;
-    const [hoverRef, isHovered] = useHover();
-    const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid)
-    const isMobile = useMediaQuery('(max-width: 992px)');
-    const canShowIcons = isMobile||isHovered;
-    
+const MessageItem = ({ message, handleAdmin, handleLike,handleDelete }) => {
+  const { author, createdAt, text, likes, likeCount } = message;
+  const isAdmin = useCurrentRoom((v) => v.isAdmin);
+  const admins = useCurrentRoom((v) => v.admins);
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
+  const [hoverRef, isHovered] = useHover();
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
+  const isMobile = useMediaQuery("(max-width: 992px)");
+  const canShowIcons = isMobile || isHovered;
+
   return (
-    <li ref={hoverRef} className={`padded mb-1 cursor-pointer ${isHovered?'bg-black-02':''}`}>
-        <div  className='d-flex align-items-center font-bolder mb-1'>
-            <PresenceDot uid={author.uid} />
-            <ProfileAvatar 
-            src={author.avatar}
-            name={author.name}
-            className="ml-1"
-            size="xs"
-            />
-            <ProfileInfoBtnModal profile={author}
-            className='ml-2 text-black'
-            >
-              {canGrantAdmin && 
-              <Button
-              block
-              onClick={()=>handleAdmin(author.uid)}
-              color="blue"
-              >
-                {isMsgAuthorAdmin?'Remove admin permission':'Grant admin permission'}
-              </Button>
-              }
-
-            </ProfileInfoBtnModal>
-            <TimeAgo 
-            datetime={createdAt}
-            className="font-normal text-black-45 ml-2"
-            />
-            <IconBtnControl 
-            {...(isLiked?({color:'red'}):({}))}
+    <li
+      ref={hoverRef}
+      className={`padded mb-1 cursor-pointer ${isHovered ? "bg-black-02" : ""}`}
+    >
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+        <ProfileInfoBtnModal profile={author} className="ml-2 text-black">
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? "Remove admin permission"
+                : "Grant admin permission"}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(isLiked ? { color: "red" } : {})}
+          isVisible={canShowIcons}
+          iconName="heart"
+          tooltip="Like this message"
+          onClick={() => handleLike(message.id)}
+          badgeContent={likeCount}
+        />
+        {isAuthor && (
+          <IconBtnControl
             isVisible={canShowIcons}
-            iconName="heart"
-            tooltip="Like this message"
-            onClick={()=>handleLike(message.id)}
-            badgeContent={(0)}
-            />
-        </div>
-        <div className="word-break-all">
-            {text}
-        </div>
+            iconName="trash"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
+      </div>
+      <div className="word-break-all">{text}</div>
     </li>
-  )
-}
+  );
+};
 
-export default memo(MessageItem)
+export default memo(MessageItem);
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,76 +1,124 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import { Alert } from 'rsuite';
-import { auth, database } from '../../../misc/firebase';
-import { transformToArrWithId } from '../../../misc/helper';
-import MessageItem from './MessageItem';
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Alert } from "rsuite";
+import { auth, database } from "../../../misc/firebase";
+import { transformToArrWithId } from "../../../misc/helper";
+import MessageItem from "./MessageItem";
 
 const Messages = () => {
-    const {chatId} = useParams();
-    const [messages, setmessages] = useState(null)
-    const isChatEmpty = messages && messages.length===0;
-    const canShowMsg = messages && messages.length>0
+  const { chatId } = useParams();
+  const [messages, setmessages] = useState(null);
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMsg = messages && messages.length > 0;
 
-    useEffect(()=>{
-        const msgRef = database.ref('messages');
+  useEffect(() => {
+    const msgRef = database.ref("messages");
 
-        msgRef.orderByChild('roomId').equalTo(chatId).on('value',snap => {
-            const data = transformToArrWithId(snap.val())
-            setmessages(data)
-        })
-        return () => {
-            msgRef.off('value')
+    msgRef
+      .orderByChild("roomId")
+      .equalTo(chatId)
+      .on("value", (snap) => {
+        const data = transformToArrWithId(snap.val());
+        setmessages(data);
+      });
+    return () => {
+      msgRef.off("value");
+    };
+  }, [chatId]);
+
+  const handleAdmin = useCallback(
+    async (uid) => {
+      const adminsRef = database.ref(`/rooms/${chatId}/admins`);
+      let alertMsg;
+      await adminsRef.transaction((admins) => {
+        if (admins) {
+          if (admins[uid]) {
+            admins[uid] = null;
+            alertMsg = "Admin permission Revoked";
+          } else {
+            admins[uid] = true;
+            alertMsg = "Admin permission Granted";
+          }
+          return admins;
         }
-    },[chatId])
+      });
+      Alert.info(alertMsg, 2000);
+    },
+    [chatId]
+  );
 
-    const handleAdmin = useCallback(async(uid)=>{
-        const adminsRef = database.ref(`/rooms/${chatId}/admins`)
-        let alertMsg;
-        await adminsRef.transaction(admins=>{
-            if(admins){
-                if(admins[uid]){
-                    admins[uid] = null;
-                    alertMsg="Admin permission Revoked";
-                }else{
-                    admins[uid]=true;
-                    alertMsg="Admin permission Granted"
-                }
-                return admins;
-            }
-        })
-        Alert.info(alertMsg,2000)
-    },[chatId])
-
-const handleLike = useCallback(async (msgId)=>{
-    const {uid} = auth.currentUser;
-    const msgRef = database.ref(`/messages/${msgId}`)
+  const handleLike = useCallback(async (msgId) => {
+    const { uid } = auth.currentUser;
+    const msgRef = database.ref(`/messages/${msgId}`);
     let alertMsg;
-    await msgRef.transaction(msg=>{
-        if(msg){
-            if(msg.likes&&msg.likes[uid]){
-                msg.likeCount -=1;
-                msg.likes[uid] = null;
-                alertMsg="Like removed";
-            }else{
-                msg.likeCount+=1;
-                if(!msg.likes){
-                    msg.likes={}
-                }
-                msg.likes[uid]=true;
-                alertMsg="Liked"
-            }
-            return msg;
+    await msgRef.transaction((msg) => {
+      if (msg) {
+        if (msg.likes && msg.likes[uid]) {
+          msg.likeCount -= 1;
+          msg.likes[uid] = null;
+          alertMsg = "Like removed";
+        } else {
+          msg.likeCount += 1;
+          if (!msg.likes) {
+            msg.likes = {};
+          }
+          msg.likes[uid] = true;
+          alertMsg = "Liked";
         }
-    })
-    Alert.info(alertMsg,2000)
-},[])
+        return msg;
+      }
+    });
+    Alert.info(alertMsg, 2000);
+  }, []);
+
+  const handleDelete = useCallback(
+    async (msgId) => {
+      if (!window.confirm("Delete this message?")) {
+        return;
+      }
+
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+
+      try {
+        await database.ref().update(updates);
+        Alert.info("Message has been deleted", 4000);
+      } catch (error) {
+        Alert.error(error.message, 4000);
+      }
+    },
+    [chatId, messages]
+  );
 
   return (
-    <ul style={{'listStyle':'none'}}>
-        {isChatEmpty&&<li>No messages yet</li>}
-        {canShowMsg&&messages.map(msg => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLike}/>)}
+    <ul style={{ listStyle: "none" }}>
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMsg &&
+        messages.map((msg) => (
+          <MessageItem
+            key={msg.id}
+            message={msg}
+            handleAdmin={handleAdmin}
+            handleLike={handleLike}
+            handleDelete={handleDelete}
+          />
+        ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
